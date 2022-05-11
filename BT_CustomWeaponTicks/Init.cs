83c2ded24b013ea0b03bb03a038e8846c4535d24
NBKRedSpy <54865934+NBKRedSpy@users.noreply.github.com>
2022-05-11 17:01:53 -0400

    Add project files.

---
+++ BT_CustomWeaponTicks/Init.cs
@@ -0,0 +1,25 @@
+﻿using Harmony;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_CustomWeaponTicks
+{
+    public static class Init
+    {
+        public static void HarmonyInit(string directory, string settingsJSON)
+        {
+            ModSettings modSettings = Newtonsoft.Json.JsonConvert.DeserializeObject<ModSettings>(settingsJSON);
+            Core.ModSettings = modSettings;
+
+            var harmony = HarmonyInstance.Create("io.github.nbk_redspy.BT_CustomWeaponTicks");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            Core.ModSettings = JsonConvert.DeserializeObject<ModSettings>(settingsJSON);
+        }
+    }
+}
---
+++ BT_CustomWeaponTicks/ModSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_CustomWeaponTicks
+{
+    public class ModSettings
+    {
+        public float xScale { get; set; } = 2;
+        public float yScale { get; set; } = 2;
+        public float zScale { get; set; } = 1;
+        public float maxIndividualScale { get; set; } = 2;
+
+    }
+}
